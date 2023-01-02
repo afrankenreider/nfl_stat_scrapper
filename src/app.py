28ead774523f8617250111131490ceb8e1eb8e49
Andrew Frankenreider <andrew.frankenreider@chrobinson.com>
2023-01-02 11:42:56 -0600

    instructions for grid

--- src/app.py
+++ src/app.py
@@ -10,12 +10,17 @@ def run_stat_scrapper():
 
     # scrape passing stats
     player_names = nfl_page.get_player_names()
+    print("Player names located!")
     player_stats = nfl_page.get_player_data()
+    print("Player data located!")
 
+    # combine player names and player stats
+    # these are two separate tables on ESPN
     stats = pd.concat([player_names, player_stats], axis=1)
     print(stats.head(5))
     logging.info("NFL stat scrapping was successful!")
 
+    # close and quit chrome driver
     nfl_page.tear_down_driver()
 
 
